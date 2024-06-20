f5cecb4766853e92d1f60092bda6790372595fee
07nainankit <ankitnain_ec21a16_07@dtu.ac.in>
2024-06-20 15:47:45 +0530

    first

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      hello world 
-    </div>
+    <>
+      <Header/>
+      <Footer/>
+    </>
   );
 }
 
