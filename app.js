83ad356576c93fdfa759efcb218c3e8674cc39ae
07nainankit <ankitnain_ec21a16_07@dtu.ac.in>
2024-10-17 15:12:57 +0530

    all get routes made

--- app.js
+++ app.js
@@ -8,15 +8,11 @@ const path = require("path");
 const auth = require("./routes/auth");
 const list = require("./routes/list");
 
-// const about = require("./routes/about");
-
 app.use(express.json());
 app.use(cors());
 
 app.use("/api/v1", auth);
 app.use("/api/v2", list);
-
-// app.use("/api/v3", about);
 
 app.get("/", (req, res) => {
   app.use(express.static(path.resolve(__dirname, "frontend", "build")));
@@ -24,6 +20,21 @@ app.get("/", (req, res) => {
 });
 
 app.get("/about", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+
+app.get("/todo", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+
+app.get("/signin", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "frontend", "build")));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
+
+app.get("/signup", (req, res) => {
   app.use(express.static(path.resolve(__dirname, "frontend", "build")));
   res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
--- routes/about.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const router = require('express').Router();
-
-// About route to serve the React app
-router.get("/about", async (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html")); // Adjust the path if needed
-});
-
-module.exports = router;
